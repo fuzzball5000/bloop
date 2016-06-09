adb17f761904090028048462bb9aba33895e8668
Cloud User <centos@ip-172-30-1-108.ec2.internal>
2016-06-09 21:17:47 +0000

    added time

--- bloop_writer.py
+++ bloop_writer.py
@@ -2,11 +2,13 @@ import requests
 import os
 import MySQLdb
 import time
+from time import strftime
 
 db_pass = os.environ['sql_pass']
 key = os.environ['met_key']
 url = 'http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/3544?res=hourly&key='
 db = MySQLdb.connect('localhost','bloop_write',db_pass,'bloop' )
+strtime = strftime("%Y-%m-%d %H:%M:%S")
 
 response = requests.get(url+key)
 epoch = int(time.time()) 
@@ -21,7 +23,7 @@ met_hydro = data['SiteRep']['DV']['Location']['Period'][0]['Rep'][0]['H']
 print (met_temp,' ',met_code, ' ',met_hydro)
 
 try:
-    cursor.execute("INSERT INTO edwin(epoc,e_temp,e_hydro,m_temp,m_code,m_hydro) VALUES(%s, %s, %s, %s, %s, %s)", (epoch, '0','0',met_temp,met_code,met_hydro))
+    cursor.execute("INSERT INTO edwin(epoc,e_temp,e_hydro,m_temp,m_code,m_hydro,datetime) VALUES(%s, %s, %s, %s, %s, %s, %s)", (epoch, '0','0',met_temp,met_code,met_hydro,strtime))
     db.commit()
 except:     
     db.rollback()
