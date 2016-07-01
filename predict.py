90862dc52f6c3a1209c1740b04a30642992b03f1
Gary <gazbo83@gmail.com>
2016-07-01 14:36:05 +0100

    case changed, resume doom

--- predict.py
+++ predict.py
@@ -31,7 +31,7 @@ cursor.close ()
 db.close()
 
 clf = linear_model.BayesianRidge()
-clf.fit(X, Y)
+clf.fit(x, y)
 BayesianRidge(alpha_1=1e-06, alpha_2=1e-06, compute_score=False, copy_X=True,
 fit_intercept=True, lambda_1=1e-06, lambda_2=1e-06, n_iter=300,
 normalize=False, tol=0.001, verbose=False)
