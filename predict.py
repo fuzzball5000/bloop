5abb3cd01953c755c0ecef4988b579a4e70adb25
Gary <gazbo83@gmail.com>
2016-11-28 09:27:23 +0000

    Extra columns

--- predict.py
+++ predict.py
@@ -16,22 +16,14 @@ except MySQLdb.Error as e:
     print("DB connect error: {}".format(e))
     sys.exit(1)
 
-cursor.execute ("select MIN(datetime),e_temp,e_hydro,m_temp,m_hydro from edwin GROUP BY DATE(datetime),HOUR(datetime) order by epoc DESC LIMIT 100")
+cursor.execute ("select MIN(datetime),e_temp,e_hydro,m_temp,m_hydro,m_code from edwin GROUP BY DATE(datetime),HOUR(datetime) order by epoc DESC LIMIT 100")
 
 data = cursor.fetchall ()
 
 c = csv.writer(open("/home/centos/bloop/temp.csv","wb"))
-c.writerow(['date','e_temp','e_hydro','m_temp','m_hydro'])
+c.writerow(['date','e_temp','e_hydro','m_temp','m_hydro','m_code'])
 for i in data:
     c.writerow(i)
 
 cursor.close ()
 db.close()
-
-##This isn't even close to working... sure beats
-##empty whitespace though...
-##clf = linear_model.BayesianRidge()
-##clf.fit(x, y)
-##BayesianRidge(alpha_1=1e-06, alpha_2=1e-06, compute_score=False, copy_X=True,
-##fit_intercept=True, lambda_1=1e-06, lambda_2=1e-06, n_iter=300,
-##normalize=False, tol=0.001, verbose=False)
