13e323a665f3c67713f2ca3c1a60d14fa6cad480
Gary <gazbo83@gmail.com>
2016-11-28 09:23:19 +0000

    Actually trying some prediction

---
+++ predict2.py
@@ -0,0 +1,21 @@
+import os
+import MySQLdb
+import time
+import sys
+from sklearn import linear_model
+import csv
+
+db_pass = os.environ['sql_pass']
+epoch = int(time.time())
+oneDay = epoch - 86400
+
+try:
+    db = MySQLdb.connect('localhost','bloop_write',db_pass,'bloop' )
+    cursor = db.cursor()
+except MySQLdb.Error as e:
+    print("DB connect error: {}".format(e))
+    sys.exit(1)
+
+cursor.execute ("select MIN(datetime),e_temp,e_hydro,m_temp,m_hydro from edwin GROUP BY DATE(datetime),HOUR(datetime) order by epoc DESC LIMIT 100")
+
+data = cursor.fetchall ()
